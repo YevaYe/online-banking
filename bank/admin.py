d7613fbc4a4181b29dc6039bf544fae0f6c3cd26
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-07-22 19:03:10 +0300

    Create custom admin panel with simple features

--- bank/admin.py
+++ bank/admin.py
@@ -1,9 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from bank.models import User, Transaction, Account, Category, Country
+from bank.models import Client, Transaction, Account, Category, Country
 
-admin.site.register(User)
+
+# admin.site.register(User)
+@admin.register(Client)
+class ClientAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("birthday", "country", "user_type",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("birthday", "country", "user_type")}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "birthday",
+                        "user_type"
+                    )
+                },
+            ),
+        )
+    )
+
+
 admin.site.register(Transaction)
+# @admin.register(Transaction)
+# class TransactionAdmin(admin.ModelAdmin):
+#     search_fields = ("account_from", "account_to", "date", "category", "amount")
+#     list_filter = ("date",),
+
+
 admin.site.register(Account)
 admin.site.register(Category)
 admin.site.register(Country)
