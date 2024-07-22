1ae91e6f173f7e0f1782b77cd8e339a7af4a5ed0
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-07-22 13:18:45 +0300

    Register models in admin.py

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from bank.models import User, Transaction, Account, Category, Country
+
+admin.site.register(User)
+admin.site.register(Transaction)
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(Country)
