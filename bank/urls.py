2dff9b3e40503026752710098f19afaa949cacf5
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-08-08 19:10:14 +0300

    Implement base views.py and urls.py

--- bank/urls.py
+++ bank/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 
-from bank.views import index
+from .views import (
+    index,
+    # AccountListView,
+    CategoryListView,
+    CountryListView,
+    TransactionListView,
+    MyBalanceView,
+)
+
+app_name = "bank"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("my-balance/", MyBalanceView.as_view(), name="my-balance"),
+    # path("balance/", AccountListView.as_view(), name="account-list"),
+    path("category/", CategoryListView.as_view(), name="category-list"),
+    path("country/", CountryListView.as_view(), name="country-list"),
+    path("transaction/", TransactionListView.as_view(), name="transaction-list"),
 ]
-
-app_name = "bank"
--- online_banking/urls.py
+++ online_banking/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", include("bank.urls", namespace="bank")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
